8efeaf3a808725e546b93f16bc95e2f8e9701d72
seuxieang <=>
2022-08-22 16:11:23 +0800

    增加带权并查集

--- UnionFindSet.py
+++ UnionFindSet.py
@@ -119,3 +119,63 @@ class UnionFindSet:
         self.father_list[node] = node
         self.size_list[node] = 1
         self.count += 1
+
+# 带权并查集
+class UnionFindSetWithValue:
+    def __init__(self):
+        """
+        记录每个节点的父节点
+        记录每个节点到根节点的权重
+        """
+        self.father = {}
+        self.value = {}
+
+    def find(self, x):
+        """
+        查找根节点
+        路径压缩
+        更新权重
+        """
+        root = x
+        # 节点更新权重的时候要放大的倍数
+        base = 1
+        while self.father[root] != None:
+            root = self.father[root]
+            base *= self.value[root]
+
+        while x != root:
+            original_father = self.father[x]
+            ##### 离根节点越远，放大的倍数越高
+            self.value[x] *= base
+            base /= self.value[original_father]
+            #####
+            self.father[x] = root
+            x = original_father
+
+        return root
+
+    def merge(self, x, y, val):
+        """
+        合并两个节点
+        """
+        root_x, root_y = self.find(x), self.find(y)
+
+        if root_x != root_y:
+            self.father[root_x] = root_y
+            ##### 四边形法则更新根节点的权重
+            self.value[root_x] = self.value[y] * val / self.value[x]
+
+    def is_connected(self, x, y):
+        """
+        两节点是否相连
+        """
+        return x in self.value and y in self.value and self.find(x) == self.find(y)
+
+    def add(self, x):
+        """
+        添加新节点，初始化权重为1.0
+        """
+        if x not in self.father:
+            self.father[x] = None
+            self.value[x] = 1.0
+
